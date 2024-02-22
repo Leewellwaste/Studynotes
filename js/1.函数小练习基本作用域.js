07b62b8d6620000c6fdc18ec86d9bd5788b6b6d9
Leewellwaste <116639380+Leewellwaste@users.noreply.github.com>
2024-02-22 23:10:32 +0800

    Add files via upload

---
+++ js/1.函数小练习基本作用域.js
@@ -0,0 +1,20 @@
+// case
+// function screm(animal){
+//     switch(animal){
+//         case "dog":
+//             document.write('wang');
+//     }   
+// }
+
+// 数字逆转输出汉字
+function nizhuan(){
+    var num = window.prompt('input');
+    var 
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+
+
+
+}
---
+++ js/2.js初步学习.js
@@ -0,0 +1,23 @@
+// console.log("hello leewell")
+
+
+// const age = 2
+// if(age > 18){
+//     console.log("🚗 is ok");
+// }else{
+//     console.log("no!")
+// }
+
+// console.log(`i am ${age}`)
+// console.log('i am',age);
+
+
+// const input = prompt("input:")
+// console.log(input)
+
+// ---switch
+const day = 'monday'
+switch(day){
+    case 'monday':
+        console.log('周一');
+} 
